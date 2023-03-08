e10d9c3c0dc40a2e0417c01b1abe151e4233674d
msamricth <emm.talarico@gmail.com>
2023-03-08 12:26:40 -0500

    package issues lead to alot of updates

--- src/_asset/js/scripts/airtable.js
+++ src/_asset/js/scripts/airtable.js
@@ -5,21 +5,113 @@ new MutationObserver((mutations) => {
     
     isVueLoaded = app.classList.contains('vue-loaded');
     if(isVueLoaded){
-        startApp();
+       startApp2();
     }
   }
 }).observe(app, { attributes: true });
 
-
 function startApp(){
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: 'patH2pzfuG2mbHzqd.18833d9077176b68bdf8f7c436376fc5d4962b4df98e82ffb1cf0ed3cedd64e5'}).base('app7o6tSJG6UICys8');
+  
+  base('Stores').select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+  
+      records.forEach(function(record) {
+          console.log('Retrieved', record.get('Number'));
+      });
+  
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+  
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
+
+}
+
+function startApp1(){
     const { createApp } = Vue
-                
-    createApp({
-        data() {
-          return {
-            message: 'Hello Vue!'
+    var settings = {
+      "url": "https://api.airtable.com/v0/app7o6tSJG6UICys8/Stores?view=API",
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "Authorization": "Bearer patH2pzfuG2mbHzqd.18833d9077176b68bdf8f7c436376fc5d4962b4df98e82ffb1cf0ed3cedd64e5",
+        "Cookie": "brw=brwaoMYe7aEFiKynU; AWSALB=IsfWQUsV1MwhumCr9Tb/4WPjsgJ5591A10gPTIYovuWll2HlrC9XqWZ8ZsLR6ZepqkjOMrRWOt1RJfYAnxjEUJ1AxVeaXRCP7dsf5LybIAqwzN3IQjT53xllUWYn; AWSALBCORS=IsfWQUsV1MwhumCr9Tb/4WPjsgJ5591A10gPTIYovuWll2HlrC9XqWZ8ZsLR6ZepqkjOMrRWOt1RJfYAnxjEUJ1AxVeaXRCP7dsf5LybIAqwzN3IQjT53xllUWYn"
+      },
+    };
+    
+    $.ajax(settings).done(function (response) {
+      
+      this.rfields = response.data;
+      var fields = this.rfields;
+      console.log(fields);
+    
+  const { createApp } = Vue
+                 
+      createApp({
+          data() {
+            return {
+              message: fields.Name
+            }
           }
-        }
-      }).mount('#app');
-      console.log('airtable');
+        }).mount('#app');
+        console.log('airtable');
+    });
+}
+function startApp2(){
+  var stores = new Vue({
+      el: '#app',
+      data () {
+      return {
+          siteTitle: null,
+          siteTag:null,
+          headerIMG: null,
+          loading: true,
+          errored: false,
+          featuredProjects: [],
+          additionalProjects: [],
+          skillsets: [],
+          rfields: []
+      }
+      },
+      filters: {
+      footerdecimal (value) {
+          return value.toFixed(2)
+      }
+      },
+      mounted () {
+  
+      axios
+          .get("https://api.airtable.com/v0/app7o6tSJG6UICys8/Stores?view=API",{ 
+              headers: { Authorization: "Bearer patH2pzfuG2mbHzqd.18833d9077176b68bdf8f7c436376fc5d4962b4df98e82ffb1cf0ed3cedd64e5"
+             } 
+          })
+          .then(response => {
+            
+              this.rfields = response.data.records;
+              var theRecords = this.rfields;
+
+	            for( var i = 0; i < theRecords.length; i++ ) {
+                console.log(theRecords[i].fields.Name);
+              }
+              console.log(fields);
+
+              
+      
+          })
+          .catch(error => {
+          console.log(error)
+          this.errored = true
+      })
+          .finally(() => this.loading = false)
+      }
+  });
 }
--- src/_asset/js/scripts/includes.js
+++ src/_asset/js/scripts/includes.js
@@ -22,8 +22,9 @@ const app = document.getElementById('app');function include(file, name, deferred
 }
 
 /* Include Many js files */
-include('https://unpkg.com/vue@3/dist/vue.global.js', 'vue');
+include('https://cdn.jsdelivr.net/npm/vue/dist/vue.js', 'vue');
 include('https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js', 'axios');
+include('https://cdn.jsdelivr.net/npm/airtable@0.11.6/lib/airtable.umd.min.js', 'airtable');
 include('https://code.jquery.com/jquery-3.6.1.min.js','jquery', true,'anonymous',"sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=");
 include('https://kit.fontawesome.com/8b0174b394.js','fontawesome', true, 'anonymous');
 
