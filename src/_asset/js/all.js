c407ed9e325941bac36be8b7cf00a86464e319da
msamricth <emm.talarico@gmail.com>
2023-06-13 14:58:12 -0400

    Facebook issue

--- src/_asset/js/all.js
+++ src/_asset/js/all.js
@@ -1,10 +1,4 @@
-function getPathFromUrl(url) {
-    return url.split(/[?#]/)[0];
-}
-const $pageURL = window.location.href
-if ($pageURL.indexOf("?fbclid") > -1) {
-    window.location.href = getPathFromUrl($pageURL)
-}
+import '.scripts/query-check.js'
 import './scripts/store-page.js';
 import './scripts/store-map.js';
 import './scripts/events.js';
--- src/_asset/js/scripts/query-check.js
+++ src/_asset/js/scripts/query-check.js
@@ -1,7 +1,7 @@
 function getPathFromUrl(url) {
     return url.split(/[?#]/)[0];
 }
-$pageURL = window.location.href
-if (pageURL.indexOf("?fbclid") > -1) {
-    window.location.href = getPathFromUrl(pageURL)
+const $pageURL = window.location.href
+if ($pageURL.indexOf("?fbclid") > -1) {
+    window.location.href = getPathFromUrl($pageURL)
 }
