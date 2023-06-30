ad1adb9d5dc1b73e0110ca96b818162faf5a46b7
msamricth <emm.talarico@gmail.com>
2023-06-30 09:05:36 -0400

    second push of preloader

--- src/_asset/js/scripts/query-check.js
+++ src/_asset/js/scripts/query-check.js
@@ -5,3 +5,29 @@ const $pageURL = window.location.href
 if ($pageURL.indexOf("?fbclid") > -1) {
     window.location.href = getPathFromUrl($pageURL)
 }
+
+const app = document.getElementById('app');
+var isVueLoaded,isAxiosLoaded,isAirtableLoaded,isJqueryLoaded,isMapboxLoaded;
+const observer = new MutationObserver((mutations) => {
+    if (mutations[0].attributeName === 'class') { 
+        
+        isVueLoaded = app.classList.contains('vue-loaded');
+        isAxiosLoaded = app.classList.contains('axios-loaded');
+        isAirtableLoaded = app.classList.contains('airtable-loaded');
+        isJqueryLoaded = app.classList.contains('jquery-loaded');
+        isMapboxLoaded = app.classList.contains('mapbox-loaded');
+
+        
+        if(isVueLoaded){
+            if(isAxiosLoaded){
+                if(isAirtableLoaded){
+                    if(isJqueryLoaded){ 
+                        if(isMapboxLoaded){
+                            document.querySelector('.preloader.storemap').style.display = "none";
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
