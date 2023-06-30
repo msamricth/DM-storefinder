ab579aa04835a9c1ce8ae0418e65950352d2a062
msamricth <emm.talarico@gmail.com>
2023-06-30 09:40:26 -0400

    4th push of preloader

--- src/_asset/js/scripts/query-check.js
+++ src/_asset/js/scripts/query-check.js
@@ -5,25 +5,4 @@ const $pageURL = window.location.href
 if ($pageURL.indexOf("?fbclid") > -1) {
     window.location.href = getPathFromUrl($pageURL)
 }
-
-const app = document.getElementById('app');
-var isVueLoaded,isAxiosLoaded,isAirtableLoaded,isJqueryLoaded,isMapboxLoaded;
-var preloader = document.querySelector('.storemap-preloader');
-const observer = new MutationObserver((mutations) => {
-    if (mutations[0].attributeName === 'class') { 
-        
-        isVueLoaded = app.classList.contains('vue-loaded');
-        isAxiosLoaded = app.classList.contains('axios-loaded');
-        isAirtableLoaded = app.classList.contains('airtable-loaded');
-        isJqueryLoaded = app.classList.contains('jquery-loaded');
-        isMapboxLoaded = app.classList.contains('mapbox-loaded');
-
-        
-                        if(isMapboxLoaded){
-                            preloader.style.display = "none";
-                            console.log('preloader should have dipped');
-                        }
-                    }
-                
-});
-observer.observe(app, { attributes: true });
+ 
