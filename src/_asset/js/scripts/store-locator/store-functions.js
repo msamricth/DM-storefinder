6b2b22c2db574079a2ec5208dfedc5d6910d98a0
Emm Talarico <emmelia@workwithsupply.com>
2023-10-25 17:11:45 -0400

    Bug fixing

--- src/_asset/js/scripts/store-locator/store-functions.js
+++ src/_asset/js/scripts/store-locator/store-functions.js
@@ -1,27 +1,26 @@
-import { app, appClassList, pageURL, storePageClass } from "./identifiers.js";
 import { startApp2 } from "./locator.js";
-
+let pageURL = window.location.href;
 var decodeEntities = (function () {
     // this prevents any overhead from creating the object each time
     var element = document.createElement('div');
-  
+
     function decodeHTMLEntities(str) {
-      if (str && typeof str === 'string') {
-        // strip script/html tags
-        str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
-        str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
-        element.innerHTML = str;
-        str = element.textContent;
-        element.textContent = '';
-      }
-  
-      return str;
+        if (str && typeof str === 'string') {
+            // strip script/html tags
+            str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+            str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+            element.innerHTML = str;
+            str = element.textContent;
+            element.textContent = '';
+        }
+
+        return str;
     }
-  
+
     return decodeHTMLEntities;
-  })();
-  
-function buildMap(){
+})();
+
+function buildMap() {
     const mapContainer = app.appendChild(document.createElement("div"));
     mapContainer.classList.add('map-container');
     const mapOBJ = mapContainer.appendChild(document.createElement("div"));
@@ -29,7 +28,7 @@ function buildMap(){
     mapOBJ.classList.add('map');
     const sidebar = mapContainer.appendChild(document.createElement("div"));
     sidebar.classList.add('sidebar');
-    
+
     const searchcntr = sidebar.appendChild(document.createElement("div"));
     searchcntr.classList.add('searchcntr');
     searchcntr.id = 'geocoder-container';
@@ -37,49 +36,59 @@ function buildMap(){
     const listings = sidebar.appendChild(document.createElement("div"));
     listings.classList.add('listings');
     listings.id = 'listings';
-    
-    window.scrollTo(0,0);
+
+    window.scrollTo(0, 0);
     setTimeout(function () {
         startApp2('push');
-      }, 400);
+    }, 400);
 }
-function returnToStore(return_map, map){
+function returnToStore(return_map, map) {
 
     const link = document.querySelector('.store-page-container').appendChild(document.createElement("a"));
     link.href = '#return-to-map';
     link.className = "store-btn";
     link.innerHTML = '<svg height="17" width="9"><polyline points="9,0,0,8.5,9,17" style="stroke:#000;fill:transparent;stroke-width:4px;"></polyline></svg>';
-    link.innerHTML +="<span>"+return_map+"</span>";
+    link.innerHTML += "<span>" + return_map + "</span>";
 
     link.addEventListener("click", function (event) {
         event.preventDefault();
         const removeStoreClasses = ['store-page', 'map-container-added', 'event-container-added'];
-        
+
         for (const removeStoreClass of removeStoreClasses) {
-            if(appClassList.contains(removeStoreClass)){
+            if (app.classList.contains(removeStoreClass)) {
                 app.classList.remove(removeStoreClass);
             }
         }
-        if(map){
+        if (map) {
             map.remove();
         }
         app.innerHTML = '';
         buildMap();
     });
 }
+let nextURL = '',
+    nextTitle = '';
+function updateStorePage(comparingSlug, page_title) {
 
-function updateStorePage(comparingSlug, page_title){
-    const nextURL = pageURL+comparingSlug;
-    const nextTitle = decodeEntities(page_title);
+
+    if (pageURL.indexOf("?") > -1) {
+        pageURL = pageURL.split("?")[0];
+    }
+    if (pageURL.indexOf("#") > -1) {
+        pageURL = pageURL.split("#")[0];
+    }
+
+    nextURL = pageURL + comparingSlug;
+    nextTitle = decodeEntities(page_title);
     const nextState = { additionalInformation: 'Updated the URL with JS' };
     // This will create a new entry in the browser's history, without reloading
     window.history.pushState(nextState, nextTitle, nextURL);
 }
 
 function LoadStoreMap(containerLon, containerLat, markerIMG, ctaIcon, return_map) {
-    
+
     const mapContainer = document.querySelector('.store-page-map-container');
-    var map,markerIMG, ctaIcon;
+    var map, markerIMG, ctaIcon;
 
     map = mapContainer.appendChild(
         document.createElement("div")
@@ -87,12 +96,12 @@ function LoadStoreMap(containerLon, containerLat, markerIMG, ctaIcon, return_map
     map.id = "map";
     map.className = "map";
 
-    var css = '#app .store-page-events-container .eb-event .event-cta:after {background-image: url('+ctaIcon+');}',
-    head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
+    var css = '#app .store-page-events-container .eb-event .event-cta:after {background-image: url(' + ctaIcon + ');}',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
     head.appendChild(style);
     style.type = 'text/css';
-    if (style.styleSheet){
+    if (style.styleSheet) {
         // This is required for IE8 and below.
         style.styleSheet.cssText = css;
     } else {
@@ -100,7 +109,7 @@ function LoadStoreMap(containerLon, containerLat, markerIMG, ctaIcon, return_map
     }
 
     mapboxgl.accessToken =
-    "pk.eyJ1IjoiY2QxNDYwIiwiYSI6ImNsZjJ4NDk1YTBjMXYzeG1sNTI4ZmprdHcifQ.quOK2CY8bs1LsIXE-BsUwg";
+        "pk.eyJ1IjoiY2QxNDYwIiwiYSI6ImNsZjJ4NDk1YTBjMXYzeG1sNTI4ZmprdHcifQ.quOK2CY8bs1LsIXE-BsUwg";
     map = new mapboxgl.Map({
         container: "map", // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
@@ -109,48 +118,48 @@ function LoadStoreMap(containerLon, containerLat, markerIMG, ctaIcon, return_map
         interactive: false,
         center: [containerLon, containerLat] // starting position
     });
-    
+
     map.on("load", () => {
         // Load an image from an external URL.
         map.loadImage(markerIMG,
-        (error, image) => {
-            if (error) throw error;
-    
-            // Add the image to the map style.
-            map.addImage("Marker", image);
-    
-            // Add a data source containing one point feature.
-            map.addSource("point", {
-            type: "geojson",
-            data: {
-                type: "FeatureCollection",
-                features: [
-                {
-                    type: "Feature",
-                    geometry: {
-                    type: "Point",
-                    coordinates: [containerLon, containerLat]
+            (error, image) => {
+                if (error) throw error;
+
+                // Add the image to the map style.
+                map.addImage("Marker", image);
+
+                // Add a data source containing one point feature.
+                map.addSource("point", {
+                    type: "geojson",
+                    data: {
+                        type: "FeatureCollection",
+                        features: [
+                            {
+                                type: "Feature",
+                                geometry: {
+                                    type: "Point",
+                                    coordinates: [containerLon, containerLat]
+                                }
+                            }
+                        ]
                     }
-                }
-                ]
+                });
+
+                // Add a layer to use the image to represent the data.
+                map.addLayer({
+                    id: "points",
+                    type: "symbol",
+                    source: "point", // reference the data source
+                    layout: {
+                        "icon-image": "Marker", // reference the image
+                        "icon-size": 0.5
+                    }
+                });
             }
-            });
-    
-            // Add a layer to use the image to represent the data.
-            map.addLayer({
-            id: "points",
-            type: "symbol",
-            source: "point", // reference the data source
-            layout: {
-                "icon-image": "Marker", // reference the image
-                "icon-size": 0.5
-            }
-            }); 
-        }
         );
     });
     map.scrollZoom.disable();
-    
+
     returnToStore(return_map, map)
 }
 
